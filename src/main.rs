57b2d88153d2ad106ade25fc764d7b05ad4a89af
eignnx <eignnx@gmail.com>
2018-01-22 12:21:21 -0500

    Test out rulinalg lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[macro_use]
+extern crate rulinalg;
+extern crate image;
+extern crate imageproc;
+
+use image::DynamicImage;
+use rulinalg::matrix::{Matrix, BaseMatrix};
+
 fn main() {
-    println!("Hello, world!");
+    // let args: Vec<String> = std::env::args().collect();
+    // let filename = args.get(1).unwrap();
+    // let original_img = image::open(filename).unwrap();
+
+    let a = Matrix::new(3, 2, vec![
+        1.0, 2.0,
+        3.0, 4.0,
+        5.0, 6.0
+    ]);
+
+    let b = &a + &a;
+    let (sigma, u, v) = a.svd().expect("SVD failed!");
+
+    println!("{}", b);
+    println!("\nsigma =\n{}\nu =\n{}\nv =\n{}", sigma, u, v);
+    println!("u . sigma . v^T =\n{}", &u * &sigma * v.transpose());
 }
